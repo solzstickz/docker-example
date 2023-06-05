94112c1b9482f43db2f55e373e4c8fedb713c7c9
pengnaja <peng.mungmontee@gmail.com>
2023-06-05 18:31:35 +0700

    tags_select

--- backend/router/tags.js
+++ backend/router/tags.js
@@ -7,38 +7,54 @@ const multer = require("multer");
 const crypto = require("crypto");
 const uploads = require("../middleware/uploads");
 
+
 router.post("/", async (req, res) => {
-  let redis_res = await redisclient.get(`/last_updated`);
-  if (redis_res) {
-    res.status(200).json(JSON.parse(redis_res));
-  } else {
-    pool.query(
-      "SELECT pages.*,posts.* FROM posts INNER JOIN pages ON posts.pages_id = pages.pages_id where posts_ep>pages.pages_last_ep-1 ORDER BY pages.pages_last_update ASC;",
-      async (err, result) => {
-        try {
-          if (err) {
-            console.log(err);
-          } else {
-            if (result.length === 0) {
-              res.status(404).json({ message: "Not Found" });
-            }
-            await redisclient.set(
-              `/last_updated`,
-              JSON.stringify(result),
-              "EX",
-              60
-            );
-            let data = await redisclient.get(`/last_updated`);
-            res.status(200).json(JSON.parse(data));
-          }
-        } catch (err) {
-          console.log(err);
-          res.status(500).json({ message: "Internal Server Error" });
-        }
+  pool.query("SELECT * FROM tags ORDER BY tags.tags_name ASC;", async (err, result) => {
+    try {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).json(result);
       }
-    );
-  }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
 });
+
+// router.post("/", async (req, res) => {
+//   let redis_res = await redisclient.get(`/last_updated`);
+//   if (redis_res) {
+//     res.status(200).json(JSON.parse(redis_res));
+//   } else {
+//     pool.query(
+//       "SELECT pages.*,posts.* FROM posts INNER JOIN pages ON posts.pages_id = pages.pages_id where posts_ep>pages.pages_last_ep-1 ORDER BY pages.pages_last_update ASC;",
+//       async (err, result) => {
+//         try {
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             if (result.length === 0) {
+//               res.status(404).json({ message: "Not Found" });
+//             }
+//             await redisclient.set(
+//               `/last_updated`,
+//               JSON.stringify(result),
+//               "EX",
+//               60
+//             );
+//             let data = await redisclient.get(`/last_updated`);
+//             res.status(200).json(JSON.parse(data));
+//           }
+//         } catch (err) {
+//           console.log(err);
+//           res.status(500).json({ message: "Internal Server Error" });
+//         }
+//       }
+//     );
+//   }
+// });
 
 router.post("/:slug", async (req, res) => {
   // console.log(req.params.slug);
