974cc092293bdcedf2c469a4f45608b5edde065f
pengnaja <peng.mungmontee@gmail.com>
2023-06-08 14:00:14 +0700

    pull_merge

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const port = process.env.SERVER_PORT;
 app.use(
   cors({
     origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   })
 );
 app.use(bodyParser.json());
