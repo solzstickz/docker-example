9a71d1ae63ddc61d7d70cb9dc5d341c9889f42d2
pengnaja <peng.mungmontee@gmail.com>
2023-06-08 14:42:06 +0700

    update_packet

--- author/config/config.tsx
+++ author/config/config.tsx
@@ -1,6 +1,6 @@
 const config = {
   ADMIN_PATH: "e923b164-6dd4-4704-b82f-5dccdaf8245c",
-  API_URL: "http://load.skz.app",
+  API_URL: "https://load.skz.app",
   CDN_URL: "http://sv1.skz.app",
 };
 
