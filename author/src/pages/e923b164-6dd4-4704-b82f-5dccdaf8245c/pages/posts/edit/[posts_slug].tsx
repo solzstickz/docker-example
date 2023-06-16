a34c9772e8c5c665685f5cc93922efafa119ed87
solzstickz <solzstickz@gmail.com>
2023-06-16 19:18:53 +0700

    no message

--- author/src/pages/e923b164-6dd4-4704-b82f-5dccdaf8245c/pages/posts/edit/[posts_slug].tsx
+++ author/src/pages/e923b164-6dd4-4704-b82f-5dccdaf8245c/pages/posts/edit/[posts_slug].tsx
@@ -152,7 +152,9 @@ export default function edit_posts({ ...props }) {
             <button
               className="flex items-center justify-between p-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
               onClick={() => {
-                router.push(`/${config.ADMIN_PATH}/posts/`);
+                router.push(
+                  `/${config.ADMIN_PATH}/pages/posts/${router.query.pages_slug}`
+                );
               }}
             >
               <FaReply className="w-3 h-3 m-2" />
