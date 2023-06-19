b834f40026164385efe525c4dbd916076175079d
solzstickz <solzstickz@gmail.com>
2023-06-19 18:28:04 +0700

    update sitemap gennareate

--- frontend/src/pages/api/sitemap.xml.tsx
+++ frontend/src/pages/api/sitemap.xml.tsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import cron from "node-cron";
-
+import moment from "moment-timezone";
 const MAX_URLS = 10;
 const SITEMAP_PATH = "./public/sitemap.xml";
 const SITEMAP_TAGS_PATH = "./public/sitemap_tags.xml";
 const SITEMAP_PAGES_PATH = "./public/sitemap_series.xml";
-const SITEMAP_POSTS_PATH = "./public/sitemap-posts-";
+const SITEMAP_POSTS_PATH = "./public/sitemap-posts";
 
 function createSitemapXml(urls: string[]): string {
   let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
@@ -26,44 +26,45 @@ function createSitemapXml(urls: string[]): string {
   return xml;
 }
 
-function sitemap_main(sitemap_posts_xml: any) {
-  let xml = `
-    <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://front.skz.app/</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-   <url>
-    <loc>https://front.skz.app/sitemap_series.xml</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-     <url>
-    <loc>https://front.skz.app/sitemap_tags.xml</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  ${sitemap_posts_xml.map((sitemap_posts_xml: any) => {
-    return `<url>
-    <loc>https://front.skz.app/sitemap-posts-${sitemap_posts_xml}.xml</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  }`;
-  })}
-  
-</urlset>
-  `;
-  return fs.writeFileSync(SITEMAP_PATH, xml);
+function updateSitemap(urls: string[], filePath: string) {
+  const sitemapXml = createSitemapXml(urls);
+  fs.writeFileSync(filePath, sitemapXml);
 }
 
-async function updateSitemap(urls: string[], filePath: string) {
-  const sitemapXml = createSitemapXml(urls);
+function updateMainSitemap(urls: string[], count: number) {
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
-  fs.writeFileSync(filePath, sitemapXml);
-
-  console.log("Sitemap updated successfully");
+  xml += `
+    <url>
+      <loc>https://front.skz.app/</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+    <url>
+      <loc>https://front.skz.app/sitemap_series.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+    <url>
+      <loc>https://front.skz.app/sitemap_tags.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `;
+  for (let i = 1; i < count; i++) {
+    xml += `
+      <url>
+        <loc>https://front.skz.app/sitemap-posts-${i}.xml</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>
+    `;
+  }
+  xml += `
+    </urlset>
+  `;
+  fs.writeFileSync(SITEMAP_PATH, xml);
 }
 
 export default async function sitemapXml(
@@ -103,13 +104,13 @@ export default async function sitemapXml(
       (slug: any) => `https://front.skz.app/${slug.posts_slug}`
     );
 
-    let sitemapPostsXml = "";
+    let sitemapPostsXml: string[] = [];
     let count = 0;
-
+    let sitemap_posts = 1;
     for (const postUrl of posts) {
       if (count % MAX_URLS === 0) {
-        if (sitemapPostsXml !== "") {
-          const filePath = `${SITEMAP_POSTS_PATH}${Math.floor(
+        if (sitemapPostsXml.length > 0) {
+          const filePath = `${SITEMAP_POSTS_PATH}-${Math.floor(
             count / MAX_URLS
           )}.xml`;
           fs.writeFileSync(filePath, createSitemapXml(sitemapPostsXml));
@@ -119,24 +120,27 @@ export default async function sitemapXml(
 
       sitemapPostsXml.push(postUrl);
       count++;
+      sitemap_posts = Math.floor(count / MAX_URLS);
     }
-    sitemap_main(count);
 
     // Write the last batch of URLs to a sitemap file
-    if (sitemapPostsXml !== "") {
+    if (sitemapPostsXml.length > 0) {
       const filePath = `${SITEMAP_POSTS_PATH}-${Math.floor(
         count / MAX_URLS
       )}.xml`;
       fs.writeFileSync(filePath, createSitemapXml(sitemapPostsXml));
     }
-    console.log("Sitemaps updated successfully");
+
+    updateMainSitemap(urls, sitemap_posts);
+
+    console.log("Sitemaps Build successfully ✅");
   } catch (error) {
     console.error("Failed to update sitemaps:", error);
   }
 }
 
 // Create a cron job that calls the updateSitemap function every hour
-cron.schedule("0 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
   try {
     const pagesResponse = await axios.get(
       "http://localhost:7777/public/sitemap/pages/slug"
@@ -158,10 +162,6 @@ cron.schedule("0 * * * *", async () => {
 
     const urls = [...pages, ...tags];
 
-    sitemap_main();
-    updateSitemap(tags, SITEMAP_TAGS_PATH);
-    updateSitemap(pages, SITEMAP_PAGES_PATH);
-
     const postsResponse = await axios.get(
       "http://localhost:7777/public/sitemap/posts/slug"
     );
@@ -171,12 +171,12 @@ cron.schedule("0 * * * *", async () => {
       (slug: any) => `https://front.skz.app/post/${slug.posts_slug}`
     );
 
-    let sitemapPostsXml = "";
+    let sitemapPostsXml: string[] = [];
     let count = 0;
-
+    let sitemap_posts = 1;
     for (const postUrl of posts) {
       if (count % MAX_URLS === 0) {
-        if (sitemapPostsXml !== "") {
+        if (sitemapPostsXml.length > 0) {
           const filePath = `${SITEMAP_POSTS_PATH}-${Math.floor(
             count / MAX_URLS
           )}.xml`;
@@ -187,17 +187,26 @@ cron.schedule("0 * * * *", async () => {
 
       sitemapPostsXml.push(postUrl);
       count++;
+      sitemap_posts = Math.floor(count / MAX_URLS);
     }
-    sitemap_main(count);
+
     // Write the last batch of URLs to a sitemap file
-    if (sitemapPostsXml !== "") {
+    if (sitemapPostsXml.length > 0) {
       const filePath = `${SITEMAP_POSTS_PATH}-${Math.floor(
         count / MAX_URLS
       )}.xml`;
       fs.writeFileSync(filePath, createSitemapXml(sitemapPostsXml));
     }
 
-    console.log("Sitemaps updated successfully");
+    updateMainSitemap(urls, sitemap_posts);
+    updateSitemap(tags, SITEMAP_TAGS_PATH);
+    updateSitemap(pages, SITEMAP_PAGES_PATH);
+
+    console.log(
+      `Sitemaps updated CronJob successfully ✅ at ${moment()
+        .tz("Asia/Bangkok")
+        .format("HH:mm:ss")}`
+    );
   } catch (error) {
     console.error("Failed to update sitemaps:", error);
   }
