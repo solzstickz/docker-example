45bd8fdbf9d22ae3941522a1a09b091c661be292
solzstickz <solzstickz@gmail.com>
2023-06-27 15:59:55 +0700

    no message

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head></Head>
+      <Head>
+        
+      </Head>
       <body>
         <Main />
         <NextScript />
