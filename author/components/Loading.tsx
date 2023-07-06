ec17facba8746f579333edf9627b8bf40acecba0
solzstickz <solzstickz@gmail.com>
2023-07-06 19:57:13 +0700

    update loading frontend

--- author/components/Loading.tsx
+++ author/components/Loading.tsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Loading() {
   return (
     <>
-      <div className="w-screen h-screen">
-        <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-site_color" />
+      <div className="layout absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex items-center justify-center">
+          <div className="border-text-color h-20 w-20 animate-spin rounded-full border-8 border-t-site_color" />
+        </div>
       </div>
     </>
   );
--- frontend/components/Loading.tsx
+++ frontend/components/Loading.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Loading() {
   return (
     <>
-      <div className="w-full h-full">
-        <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-purple-700" />
+     <div className="flex items-center justify-center w-screen h-screen">
+                          <div className="border-text-color h-20 w-20 animate-spin rounded-full border-8 border-t-site_color" />
       </div>
     </>
   );
